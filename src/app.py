742ba4689b61bd2305749c24fd6f0addde9832d3
catalinasehrt <catalina.dali94@gmail.com>
2023-03-09 14:00:46 +0000

    listo

--- src/app.py
+++ src/app.py
@@ -1,51 +1,57 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-import os
-from flask import Flask, request, jsonify, url_for
-from flask_migrate import Migrate
-from flask_swagger import swagger
-from flask_cors import CORS
-from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
-#from models import Person
+from app import app
+from flask import Flask, jsonify, request, Blueprint
+from .models import db, User, Planet, Character, FavoriteCharacter, FavoritePlanet
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+@app.route('/users', methods=['GET'])
+def getUser():
+   result = User.query.all()
+   data = list(map(lambda dataUser: dataUser.serialize(), result))
+   return jsonify(data), 200
 
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+@app.route('/characters', methods=['GET'])
+def getCharacter():
+   result = Character.query.all()
+   data = list(map(lambda dataUser: dataUser.serialize(), result))
+   return jsonify(data), 200
 
-MIGRATE = Migrate(app, db)
-db.init_app(app)
-CORS(app)
-setup_admin(app)
+@app.route('/planets', methods=['GET'])
+def getPlanet():
+   result = Planet.query.all()
+   data = list(map(lambda dataUser: dataUser.serialize(), result))
+   return jsonify(data), 200
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
+@app.route('/favorite/characters', methods=['POST'])
+def saveFavoriteCharacter():
+   data_request = request.get_json()
+   result = FavoriteCharacter(characters_id=data_request['characters_id'],create_at=data_request['create_at'])
+   db.session.add(result)
+   db.session.commit()
+   return jsonify("favorite character successfully created"), 200
 
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+@app.route('/favorite/characters/<int:id>', methods=['DELETE'])
+def deleteFavoriteCharacter(id):
+   print(id,'aloha')
+   deleteFavorite = FavoriteCharacter.query.filter_by(id=id).first()
+   if not deleteFavorite:
+      return jsonify({'message': 'Id not found'})
+   db.session.delete(deleteFavorite)
+   db.session.commit()
+   return jsonify({'message': 'The favorite has been deleted'})
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/favorite/planets', methods=['POST'])
+def saveFavoritePlanet():
+   data_request = request.get_json()
+   result = FavoritePlanet(planets_id=data_request['planets_id'],create_at=data_request['create_at'])
+   db.session.add(result)
+   db.session.commit()
+   return jsonify("favorite planets successfully created"), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
-
-# this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+@app.route('/favorite/planets/<int:id>', methods=['DELETE'])
+def deleteFavoritePlanet(id):
+   print(id,'aloha')
+   deleteFavorite = FavoritePlanet.query.filter_by(id=id).first()
+   if not deleteFavorite:
+      return jsonify({'message': 'Id not found'})
+   db.session.delete(deleteFavorite)
+   db.session.commit()
+   return jsonify({'message': 'The favorite has been deleted'})
